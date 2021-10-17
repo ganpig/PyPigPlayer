57bd6502e820d08fab6d6e73e717324cc1049a3a
Frank Gan <ganpig@qq.com>
2021-10-18 06:44:56 +0800

    Delete ppp_file.py

--- ppp_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_folder(path):
-    dirs = []
-    files = []
-    for name in os.listdir(path):
-        (dirs if os.path.isdir(os.path.join(path, name)) else files).append(name)
-    return sorted(dirs), sorted([i for i in files if i[-4:] == '.mp3'])
